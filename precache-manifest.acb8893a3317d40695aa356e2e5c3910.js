a3eb90fa00cd1852f4c76cc95bd6d1dbf4b007b8
Антон Халимонов <a.xalimonov@mail.ru>
2020-05-11 22:58:03 +0900

    Updates

---
+++ precache-manifest.acb8893a3317d40695aa356e2e5c3910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "974fa4e8528677c859b2ea046b4e49d1",
+    "url": "/JShomework/index.html"
+  },
+  {
+    "revision": "3f30ef725e1f095e36b3",
+    "url": "/JShomework/static/css/main.16c8f39b.chunk.css"
+  },
+  {
+    "revision": "6a38e3a55a591c115a13",
+    "url": "/JShomework/static/js/2.cd9c5fd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JShomework/static/js/2.cd9c5fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f30ef725e1f095e36b3",
+    "url": "/JShomework/static/js/main.d3be5355.chunk.js"
+  },
+  {
+    "revision": "73b700985c37e0b7770c",
+    "url": "/JShomework/static/js/runtime-main.6006a0ee.js"
+  }
+]);
