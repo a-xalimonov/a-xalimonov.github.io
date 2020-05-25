b0031bf89c06442e291200f9b489af90b566b58d
a-xalimonov <56163332+a-xalimonov@users.noreply.github.com>
2020-05-26 02:14:22 +0900

    Registration in Homework added

--- Homework/src/components/Header/Header.jsx
+++ Homework/src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Header.css';
 import HeaderIcon from '../../images/Header-icon.png'
+import {
+    Link
+} from 'react-router-dom'
 
 class Header extends React.Component {
     render() {
@@ -11,8 +14,8 @@ class Header extends React.Component {
                     <p className="Header-line">КУРС ВАЛЮТ</p>
                 </div>
                 <div className="Authorization">
-                    <div className="Login-button">Вход  </div>
-                    <div className="Register-button">Зарегестрироваться</div>
+                <div className="Login-button"><Link className="Link" to="/JS_Homework/Login">Вход</Link></div>
+                <div className="Register-button"><Link className="Link"  to="/JS_Homework/Registration">Зарегистрироваться</Link></div>
                 </div>
             </div>
         );
